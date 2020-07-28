63b1645b4fd9fb5bba0cc81fe9e27c64b6f5a8e1
miraszh2020 <63465408+miraszh2020@users.noreply.github.com>
2020-07-29 01:01:52 +0600

    79

--- main.js
+++ main.js
@@ -105,8 +105,10 @@ var dinosaurs = [
     dinosaurs[0];
     { name: "Тираннозавр рекс", period: "Верхнемеловой" }
 */
-
-
+var anna = { name: "Анна", age: 11, luckyNumbers: [2, 4, 8, 16] };
+var dave = { name: "Дэйв", age: 5, luckyNumbers: [3, 9, 40] };
+var kate = { name: "Кейт", age: 9, luckyNumbers: [1, 2, 3] };
+var friends = [anna,dave,kate];
 
 
 
