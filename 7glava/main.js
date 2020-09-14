1a56184e4b0e18e9a86690f87b728d59a258a302
miraszh2020 <63465408+miraszh2020@users.noreply.github.com>
2020-09-15 03:14:35 +0600

    practiceJS60' 15-09-20

--- 7glava/main.js
+++ 7glava/main.js
@@ -1,2 +1,30 @@
-var name = prompt('whats you name?');
-console.log('hi' + name);
+let aText = document.querySelector('.container');
+let ht = (ii) => {
+    for (let i = 0; i <ii; i++ ){
+    let el = document.createElement('h1');
+    let inputEl = document.createElement('input');
+    let buttonEl = document.createElement('button');
+
+        el.className = 'el';
+        el.textContent = 'JavaScript';
+    aText.appendChild(el)
+    aText.appendChild(inputEl);
+    aText.appendChild(buttonEl);
+        el.style.border = '1px solid red';
+        el.style.width ='200px';
+        el.style.textAlign = 'center'
+        buttonEl.style.width = '70px';
+        buttonEl.style.height = '18px';
+        buttonEl.style.marginLeft='5px';
+        buttonEl.textContent = 'send';
+    let hText = document.querySelector('.hhhh');
+    hText.textContent ='YAY' ;
+
+    let valueT = () => {
+        hText.textContent = inputEl.value;
+    }
+    buttonEl.addEventListener('click',valueT)
+}
+}
+ht(1);
+ht(1);
