7dda7bf6b00c119e4d99419d0b4badda7705f3f7
mikos8 <65290147+mikos8@users.noreply.github.com>
2020-09-19 22:03:50 +0600

    123 str JSK

--- vis/main.js
+++ vis/main.js
@@ -1,39 +1,86 @@
 var name = prompt("what is your name?");
 console.log("Hi, " + name);
-var likeIt = confirm("Do you like Cats ?");
-if (likeIt) {
-  console.log("you like cat's");
-} else {
-  console.log("You DON'T like ");
-}
 
 var words = [
-  "машина",
-  "город",
-  "миллион",
-  "вертолет",
-  "квартира",
-  "смартфон",
-  "планшет",
-  "карьера",
+  "auto",
+  "astana",
+  "arbalete",
+  "vertical",
+  "car",
+  "bar",
+  "parametr",
+  "dollar",
 ];
 
 var word = words[Math.floor(Math.random() * words.length)];
-
-var answerArray = [];
+var wa;
+var answerArray = []; //pustoi massiv dlya otveta b pokaza otgadannyh bukv
 for (let i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
+  answerArray[i] = "_"; //prisvaevaem kazhdomu elementu _
 }
 
-var remainingLetters = word.length;
+var remainingLetters = word.length; // peremennaya sk-ko otgadali bukv
+
 while (remainingLetters > 0) {
-  alert(answerArray.join(" "));
+  alert(answerArray.join(" ")); // delaem razdelenei probelob v massive
 
   var guess = prompt("Угадай букву или нажми отмена для выхода из игры");
 
-  if (guess === null) {
+  if (guess === null || guess === 1) {
     break;
   } else if (guess.length !== 1) {
-    alertcx
+    alert("Пожалуйста введите только 1 букву.");
+  } else {
+    for (var j = 0; j < word.length - 1; j++) {
+      if (word[j] === guess) {
+        answerArray[j] = guess;
+        remainingLetters--;
+        wa = answerArray[j];
+        console.log(guess);
+      }
+    }
   }
 }
+
+alert(
+  answerArray.join(" ") + " Отлично!" + name + " Было загадо слово >> " + word
+);
+/*
+// Создаем массив со словами
+var words = ["astana", "arbalet", "car", "auto", "camry"];
+// Выбираем случайное слово
+var word = words[Math.floor(Math.random() * words.length)];
+// Создаем итоговый массив
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+  answerArray[i] = "_";
+}
+var remainingLetters = word.length;
+// Игровой цикл
+while (remainingLetters > 0) {
+  // Показываем состояние игры
+  alert(answerArray.join(" "));
+  // Запрашиваем вариант ответа
+  var guess = prompt("Угадайте букву, или нажмите Отмена для выхода из игры.");
+  if (guess === null) {
+    // Выходим из игрового цикла
+    break;
+  } else if (guess.length !== 1) {
+    alert("Пожалуйста, введите одиночную букву.");
+  } else {
+    // Обновляем состояние игры
+    for (var j = 0; j < word.length; j++) {
+      debugger;
+      if (word[j] === guess) {
+        answerArray[j] = guess;
+        remainingLetters--;
+      }
+    }
+  }
+  // Конец игрового цикла
+}
+// Отображаем ответ и поздравляем игрока
+alert(answerArray.join(" "));
+alert("Отлично! Было загадано слово " + word);
+
+*/
