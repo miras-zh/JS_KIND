cfc7ab3af892dde6aeab15a46e7f12b7740285b1
mikos8 <65290147+mikos8@users.noreply.github.com>
2020-09-27 22:07:44 +0600

    add1upr function random

--- 8 function/main.js
+++ 8 function/main.js
@@ -8,7 +8,7 @@ function randomWords(words) {
 let callFunc = () => {
   randomWords(arrWords);
 };
-
+//Data
 let arrWords = [
   "React",
   "JS",
@@ -23,7 +23,7 @@ let arrWords = [
 let hEl = document.createElement("h1");
 hEl.className = "hhh";
 divEl.appendChild(hEl);
-
+//styleContainer
 divEl.style.width = "600px";
 divEl.style.height = "200px";
 divEl.style.textAlign = "center";
@@ -33,3 +33,35 @@ divEl.style.margin = "0 auto";
 buttonRandom.addEventListener("click", callFunc);
 
 //document.write(randomWords(arrWords));
+
+let divElTwo = document.querySelector(".newRandomContainer");
+let buttonRandomTwo = document.querySelector(".buttonRandomCompl");
+let hElTwo = document.createElement("h1");
+divElTwo.appendChild(hElTwo);
+//style
+buttonRandomTwo.style.marginTop = "20px";
+hElTwo.className = "hhh";
+divElTwo.style.width = "600px";
+divElTwo.style.height = "200px";
+divElTwo.style.textAlign = "center";
+divElTwo.style.margin = "0 auto";
+divElTwo.style.border = "1px solid black";
+
+function MyCompliment() {
+  let arrayParts = ["JS", "PHP", "Swift", "C++", "Python"];
+  let arrayOpisani = [
+    "Быстрая Ракета",
+    "Ползающая улитка",
+    " Скоростная магистраль",
+    " Севший Элетромобиль",
+    " Атомная реактор",
+  ];
+  let randomWord =
+    "Язык программирования " +
+    arrayParts[Math.floor(Math.random() * arrayParts.length)] +
+    " словно " +
+    arrayOpisani[Math.floor(Math.random() * arrayOpisani.length)];
+  return (hElTwo.textContent = randomWord);
+}
+
+buttonRandomTwo.addEventListener("click", MyCompliment);
